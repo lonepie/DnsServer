452d8f608411aef84c3802d5bd12bcec716d11d5
Jon Rogers <jon@devnullvoid.space>
2022-11-30 17:59:06 -0800

    scaffold QueryLogsElasticApp

---
+++ Apps/QueryLogsElasticApp/App.cs
@@ -0,0 +1,39 @@
+﻿using DnsServerCore.ApplicationCommon;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using TechnitiumLibrary.Net.Dns;
+using TechnitiumLibrary.Net.Dns.ResourceRecords;
+
+namespace QueryLogsElastic
+{
+    public class App : IDnsApplication, IDnsQueryLogger
+    {
+        public string Description => throw new NotImplementedException();
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task InitializeAsync(IDnsServer dnsServer, string config)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task InsertLogAsync(DateTime timestamp, DnsDatagram request, IPEndPoint remoteEP, DnsTransportProtocol protocol, DnsDatagram response)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DnsLogPage> QueryLogsAsync(long pageNumber, int entriesPerPage, bool descendingOrder, DateTime? start, DateTime? end, IPAddress clientIpAddress, DnsTransportProtocol? protocol, DnsServerResponseType? responseType, DnsResponseCode? rcode, string qname, DnsResourceRecordType? qtype, DnsClass? qclass)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
